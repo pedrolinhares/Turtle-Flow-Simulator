8c92edfb391b727d203357fe51073866aaf46987
Pedro Henrique Linhares <pedrolmota@gmail.com>
2014-01-15 13:44:27 -0200

    removido codigo nao utilizado em grid.h e feita documentação

--- src/grid.cpp
+++ src/grid.cpp
@@ -1,36 +1,7 @@
+/** \file grid.cpp */
+
 #include <QtGui>
-#include <QMessageBox>
-#include <iostream>
 #include "grid.h"
 
+/** \brief basic constructor, takes number of rows, columns and parent pointer */
 Grid::Grid(int row, int column, QWidget* parent) : QTableWidget(row, column, parent) {}
-
-bool Grid::isBeingEdited() {
-	std::cout << this->state();
- if (this->state() == QAbstractItemView::EditingState)
-   return true;
- else
-   return false;
-};
-
-bool Grid::okToContinue () {
- if (isBeingEdited()){
- 	int r = QMessageBox::warning(this, tr("Save Changes?"),
-                        tr("The document has been modified. \n"
-                        "Do you want to save your changes?"),
-                        QMessageBox::Yes | QMessageBox::Default,
-                        QMessageBox::No,
-                        QMessageBox::Cancel | QMessageBox::Escape);
-    if (r == QMessageBox::No)
-    	return true;
-    else if (r == QMessageBox::Yes)
-    	saveGrid();
-    else
-      return false;
- } else
-     return true;
-}
-
-bool Grid::saveGrid() {
-
-}
--- src/grid.h
+++ src/grid.h
@@ -1,20 +1,20 @@
+/** \file grid.h */
+
 #ifndef GRID_H
 #define GRID_H
 
 #include <QTableWidget>
 
+/** \class Grid
+** Class that represents a basic table grid.
+*/
 class Grid: public QTableWidget {
   Q_OBJECT
 public:
   explicit Grid(QWidget *parent = 0) : QTableWidget(parent){};
   Grid(int row, int column, QWidget* parent = 0);
+  ///Destructor
   ~Grid(){};
-  bool okToContinue();
-
-private:
-  bool isBeingEdited();
-  bool saveGrid();
 };
 
-
-#endif
+#endif // GRID_H
