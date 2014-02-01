361a3103898a04685a81bd8ea35c9ab50c6f0b20
Pedro Henrique Linhares <pedrolmota@gmail.com>
2014-02-01 12:24:29 -0200

    alterado caminhos dos arquivos de leitura e escrita

--- src/interface/kernel_definitions.h
+++ src/interface/kernel_definitions.h
@@ -4,17 +4,17 @@
 #ifdef __linux
 
 namespace kernel_definitions {
-  const char* GEOMETRY_FILENAME = "kernel/ArqCell1d.dat";
-  const char* BLOCK_FILENAME = "kernel/ArqBlock.dat";
-  const char* FLUID_WATER_FILENAME = "kernel/ArqWater.dat";
-  const char* FLUID_OIL_FILENAME = "kernel/ArqOil.dat";
-  const char* FLUID_GAS_FILENAME = "kernel/ArqGas.dat";
-  const char* WELLS_FILENAME = "kernel/ArqWell1d.dat";
-  const char* SOLUTION_FILENAME = "kernel/ArqInit1d.dat";
-  const char* RESERVOIR_FILENAME = "kernel/ArqRes1d.dat";
-  const char* NUMERIC_PROPERTIES_FILENAME = "kernel/ArqNumeric.dat";
-  const char* BOUNDARY_CONDITION_FILENAME = "kernel/ArqBoundCond1d.dat";
-  const char* KERNEL_COMMAND = "kernel/ReservoirSoftware";
+  const char* GEOMETRY_FILENAME = "reservoir_data/ArqCell1d.in";
+  const char* BLOCK_FILENAME = "reservoir_data/ArqBlock.in";
+  const char* FLUID_WATER_FILENAME = "reservoir_data/ArqWater.in";
+  const char* FLUID_OIL_FILENAME = "reservoir_data/ArqOil.in";
+  const char* FLUID_GAS_FILENAME = "reservoir_data/ArqGas.in";
+  const char* WELLS_FILENAME = "reservoir_data/ArqWell1d.in";
+  const char* SOLUTION_FILENAME = "reservoir_data/ArqInit1d.in";
+  const char* RESERVOIR_FILENAME = "reservoir_data/ArqRes1d.in";
+  const char* NUMERIC_PROPERTIES_FILENAME = "reservoir_data/ArqNumeric.in";
+  const char* BOUNDARY_CONDITION_FILENAME = "reservoir_data/ArqBoundCond1d.in";
+  const char* KERNEL_COMMAND = "ReservoirSoftware";
 }
 
 #endif
@@ -23,17 +23,17 @@ namespace kernel_definitions {
 #ifdef _WIN32
 
 namespace kernel_definitions {
-  const char* GEOMETRY_FILENAME = "kernel/ArqCell1d.dat";
-  const char* BLOCK_FILENAME = "kernel/ArqBlock.dat";
-  const char* FLUID_WATER_FILENAME = "kernel/ArqWater.dat";
-  const char* FLUID_OIL_FILENAME = "kernel/ArqOil.dat";
-  const char* FLUID_GAS_FILENAME = "kernel/ArqGas.dat";
-  const char* WELLS_FILENAME = "kernel/ArqWell1d.dat";
-  const char* SOLUTION_FILENAME = "kernel/ArqInit1d.dat";
-  const char* RESERVOIR_FILENAME = "kernel/ArqRes1d.dat";
-  const char* NUMERIC_PROPERTIES_FILENAME = "kernel/ArqNumeric.dat";
-  const char* BOUNDARY_CONDITION_FILENAME = "kernel/ArqBoundCond1d.dat";
-  const char* KERNEL_COMMAND = "../kernel/ReservoirSoftware";
+  const char* GEOMETRY_FILENAME = "reservoir_data/ArqCell1d.in";
+  const char* BLOCK_FILENAME = "reservoir_data/ArqBlock.in";
+  const char* FLUID_WATER_FILENAME = "reservoir_data/ArqWater.in";
+  const char* FLUID_OIL_FILENAME = "reservoir_data/ArqOil.in";
+  const char* FLUID_GAS_FILENAME = "reservoir_data/ArqGas.in";
+  const char* WELLS_FILENAME = "reservoir_data/ArqWell1d.in";
+  const char* SOLUTION_FILENAME = "reservoir_data/ArqInit1d.in";
+  const char* RESERVOIR_FILENAME = "reservoir_data/ArqRes1d.in";
+  const char* NUMERIC_PROPERTIES_FILENAME = "reservoir_data/ArqNumeric.in";
+  const char* BOUNDARY_CONDITION_FILENAME = "reservoir_data/ArqBoundCond1d.in";
+  const char* KERNEL_COMMAND = "ReservoirSoftware";
 }
 
 #endif
--- src/kernel/kernel_definitions.cpp
+++ src/kernel/kernel_definitions.cpp
@@ -2,36 +2,36 @@
 
 #ifdef __linux
 
-std::string definitions::ARQ_BLOCKS_FILE = "kernel/ArqBlock.dat";
-std::string definitions::ARQ_NUMERICS_FILE = "kernel/ArqNumeric.dat";
-std::string definitions::ARQ_RESERVOIR_FILE = "kernel/ArqRes1d.dat";
-std::string definitions::ARQ_WATER_FILE = "kernel/ArqWater.dat";
-std::string definitions::ARQ_OIL_FILE = "kernel/ArqOil.dat";
-std::string definitions::ARQ_GAS_FILE = "kernel/ArqGas.dat";
-std::string definitions::ARQ_CELL1D_FILE = "kernel/ArqCell1d.dat";
-std::string definitions::ARQ_WELL1D_FILE = "kernel/ArqWell1d.dat";
-std::string definitions::ARQ_BOUNDARY_FILE = "kernel/ArqBoundCond1d.dat";
-std::string definitions::ARQ_INIT_FILE = "kernel/ArqInit1d.dat";
+std::string definitions::ARQ_BLOCKS_FILE = "reservoir_data/ArqBlock.in";
+std::string definitions::ARQ_NUMERICS_FILE = "reservoir_data/ArqNumeric.in";
+std::string definitions::ARQ_RESERVOIR_FILE = "reservoir_data/ArqRes1d.in";
+std::string definitions::ARQ_WATER_FILE = "reservoir_data/ArqWater.in";
+std::string definitions::ARQ_OIL_FILE = "reservoir_data/ArqOil.in";
+std::string definitions::ARQ_GAS_FILE = "reservoir_data/ArqGas.in";
+std::string definitions::ARQ_CELL1D_FILE = "reservoir_data/ArqCell1d.in";
+std::string definitions::ARQ_WELL1D_FILE = "reservoir_data/ArqWell1d.in";
+std::string definitions::ARQ_BOUNDARY_FILE = "reservoir_data/ArqBoundCond1d.in";
+std::string definitions::ARQ_INIT_FILE = "reservoir_data/ArqInit1d.in";
 
-std::string definitions::OUT_GRIDS_FILE = "kernel/Out_GridPressure.out";
-std::string definitions::OUT_WELLS_FILE = "kernel/Out_Well_";
+std::string definitions::OUT_GRIDS_FILE = "reservoir_data/Out_GridPressure.out";
+std::string definitions::OUT_WELLS_FILE = "reservoir_data/Out_Well_";
 
 #endif
 
 #ifdef _WIN32
 
-std::string definitions::ARQ_BLOCKS_FILE = "kernel/ArqBlock.dat";
-std::string definitions::ARQ_NUMERICS_FILE = "kernel/ArqNumeric.dat";
-std::string definitions::ARQ_RESERVOIR_FILE = "kernel/ArqRes1d.dat";
-std::string definitions::ARQ_WATER_FILE = "kernel/ArqWater.dat";
-std::string definitions::ARQ_OIL_FILE = "kernel/ArqOil.dat";
-std::string definitions::ARQ_GAS_FILE = "kernel/ArqGas.dat";
-std::string definitions::ARQ_CELL1D_FILE = "kernel/ArqCell1d.dat";
-std::string definitions::ARQ_WELL1D_FILE = "kernel/ArqWell1d.dat";
-std::string definitions::ARQ_BOUNDARY_FILE = "kernel/ArqBoundCond1d.dat";
-std::string definitions::ARQ_INIT_FILE = "kernel/ArqInit1d.dat";
+std::string definitions::ARQ_BLOCKS_FILE = "reservoir_data/ArqBlock.in";
+std::string definitions::ARQ_NUMERICS_FILE = "reservoir_data/ArqNumeric.in";
+std::string definitions::ARQ_RESERVOIR_FILE = "reservoir_data/ArqRes1d.in";
+std::string definitions::ARQ_WATER_FILE = "reservoir_data/ArqWater.in";
+std::string definitions::ARQ_OIL_FILE = "reservoir_data/ArqOil.in";
+std::string definitions::ARQ_GAS_FILE = "reservoir_data/ArqGas.in";
+std::string definitions::ARQ_CELL1D_FILE = "reservoir_data/ArqCell1d.in";
+std::string definitions::ARQ_WELL1D_FILE = "reservoir_data/ArqWell1d.in";
+std::string definitions::ARQ_BOUNDARY_FILE = "reservoir_data/ArqBoundCond1d.in";
+std::string definitions::ARQ_INIT_FILE = "reservoir_data/ArqInit1d.in";
 
-std::string definitions::OUT_GRIDS_FILE = "kernel/Out_GridPressure.out";
-std::string definitions::OUT_WELLS_FILE = "kernel/Out_Well_";
+std::string definitions::OUT_GRIDS_FILE = "reservoir_data/Out_GridPressure.out";
+std::string definitions::OUT_WELLS_FILE = "reservoir_data/Out_Well_";
 
 #endif
